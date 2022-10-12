99a3b93976270004739b46b5c12841e6a46f4996
korobasssss <112711112+korobasssss@users.noreply.github.com>
2022-10-13 01:13:03 +0300

    Создала окно с примерным расположением поля и кнопок с вводом

--- src/cs/vsu/ruKorobeynikova_A_V/Main.java
+++ src/cs/vsu/ruKorobeynikova_A_V/Main.java
@@ -3,6 +3,6 @@ package cs.vsu.ruKorobeynikova_A_V;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        new MainForm().setVisible(true);
     }
 }
---
+++ src/cs/vsu/ruKorobeynikova_A_V/MainForm.java
@@ -0,0 +1,31 @@
+package cs.vsu.ruKorobeynikova_A_V;
+
+import ru.vsu.cs.util.SwingUtils;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainForm extends JFrame{
+    private JPanel panelMain;
+    private JTextField textFieldScale;
+    private JTextField textFieldRotate;
+    private JTextField textFieldCompression;
+    private JTextField textFieldTransfer;
+    private JButton buttonClean;
+    private JButton buttonScale;
+    private JButton buttonRotate;
+    private JButton buttonCompression;
+    private JButton buttonTransfer;
+
+    public MainForm() {
+        this.setTitle("Affine transformations");
+        this.setContentPane(panelMain);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.pack();
+        this.setLocationRelativeTo(null);
+        this.pack();
+        this.setSize(new Dimension(1300,800));
+
+
+    }
+}
